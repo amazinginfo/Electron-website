c42b62e9a7c95b69686b536de40df554fa554a6e
somesh lakhe <someshlakhe.official@gmail.com>
2022-07-26 15:07:54 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$('.fa-bars').click(function() {
+    $('.btn').toggleClass("click")
+    $('.slidebar').toggleClass("active")
+});
+
+$('.catbtn').click(function() {
+    $('.slidebar ul ul').toggleClass("click")
+    $('.catbtn').toggleClass("rotate")
+});
+
+
